b4375659fd36896a42bfe556c55429caf995bdb0
Marco Germanier <85969120+pensivecoding@users.noreply.github.com>
2022-05-16 23:01:24 +0000

    begun work on the startGame() function

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-function cambia_texto() {
-  const tit = document.getElementById('a_title')
-
-  tit.style.color = 'red'
-  tit.style.fontFamily= 'Liberation Mono'
-  tit.style.fontStyle= 'italic'
-  tit.innerHTML = "Haloo!!!"
+function createRandomArray(x) {
+  let simonArray = []
+  for (let i=x; i > 0; i--) {
+    binary = Math.round(Math.random()).toString() + Math.round(Math.random()).toString()
+    simonArray+= parseInt(binary, 2)
+  }
+  return simonArray
 }
+
+function displayArray(array) {
+  for (const element of array) {
+    console.log(element)
+  }
+}
+
+function playGame() {
+  for (x=3; x>0; i++) {
+    var simonArray = createRandomArray(i)
+    displayArray(simonArray)
+    }
+  }
 
 function clickFunction(x) {
-  console.log(x)
+  clickNumber = x[0]
+  
 }
+
+//funcion comienze el juego
+// ranInt 0-3 {} i++
+// []
+// mostrar secuencia
+//0-3
+//game.close()
+// your score is i
+// math.random(0,3)
+
