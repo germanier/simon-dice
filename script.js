362ef683683e823ecc322098e8a4b75f5af16c12
Marco Germanier <85969120+pensivecoding@users.noreply.github.com>
2022-05-30 20:42:58 +0000

    added endGame functionality

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+
 pos = 0; length = 0
 simonArray = createRandomArray(length)
+endisButton(true)
 
+// creates the SimonArray 
 function createRandomArray(x) {
-  if (x >= 3) {
+  if (x >= 3) { // if already created, add another item
     x++
     simonArray = Array.from(simonArray)
     binary = Math.round(Math.random()).toString() + 
@@ -10,7 +13,7 @@ function createRandomArray(x) {
     simonArray.push(parseInt(binary, 2))
                     
     console.info(x, simonArray)
-  } else {
+  } else { // else create array of length 3
     simonArray = []
     for (let i = 3; i > 0; i--) {
       binary = Math.round(Math.random()).toString() + 
@@ -21,11 +24,16 @@ function createRandomArray(x) {
   }
 }
 
+// wait on each  highlighted item
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// show simonArray this round
 async function displayArray() {
+  //flag_display = true
+  endisButton(true)
+  
   for (let i = 0; i < (simonArray.length); i++) {
     element = simonArray[i]
     let previousColour = document.getElementById(element).style.backgroundColor
@@ -34,18 +42,32 @@ async function displayArray() {
     document.getElementById(element).style.backgroundColor = previousColour
     await sleep(100)
   }
+
+  endisButton(false)
 }
 
-function clickFunction(click) {
+// enable / disable buttons
+function endisButton(x) {
+  var botones = document.getElementsByClassName("bot")
+  
+  for(boton of botones) {
+    boton.disabled = x 
+  }
+}
+
+// compare each click to simonArray
+function clickFunction(click) { 
   if (click != simonArray[pos]) {
     endGame()
-  } else
+    //return
+  } else {
     pos++
-
-  if (pos == simonArray.length) {
-    createRandomArray(pos)
-    pos = 0
-    newRound()
+    if (pos == simonArray.length) {
+      createRandomArray(pos)
+      pos = 0
+      endisButton(true)
+      newRound()
+    }
   }
 }
 
@@ -55,8 +77,44 @@ function newRound() {
   //  newRound(length++)
 }
 
+function loseButtonClick() {
+  document.getElementById('losePopup').style.visibility = 'hidden';
+
+}
+
+var flg_responseok = false
+
+function errorornot() {
+  if (!flg_responseok) {
+    document.getElementById('loseImage').src = 'dumbimg.jpg'
+  }
+}
+
 function endGame() {
- 
+  endisButton(true)
+  var url_api = "https://dog.ceo/api/breeds/image/random"
+  flg_responseok = false
+  setTimeout(errorornot, 1000)
+  
+  fetch(url_api).then(     
+    response => {
+      if (response.ok) {
+        return response.text()
+      } else {
+        //throw new Error(`HTTP error: ${response.status}`);
+        return "{'message':'dumbimg.jpg'}"
+      }
+    }     
+  ).then( 
+      t => { 
+        document.getElementById('loseImage').src = JSON.parse(t).message
+        
+        document.getElementById('losePopup').style.visibility = 'visible';
+        flg_responseok = true
+      }
+  ).catch(console.log('pipo'))
+  
+  simonArray = []
   
 }
 
